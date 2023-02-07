8787c8337eca1a22a0adb19a200f03bda0cf8ec0
sinEscanor <116595305+sinEscanor@users.noreply.github.com>
2023-02-07 23:24:30 +0530

    Update Wave print problem.cpp

--- 2 D Array/Wave print problem.cpp
+++ 2 D Array/Wave print problem.cpp
@@ -1,1 +1,18 @@
-
+void wavePrint(int arr[][4], int row , int col){
+    for (int i=0;i<col;i++){
+        int start=0;
+        int last = row-1;
+        if(i%2 ==0){
+            for (start; start<=last;start++){ 
+                cout << arr[start][i] << " ";
+            }
+            
+        }
+        else{
+            for (last; last>=start; last--){
+                cout << arr[last][i] << " ";
+            }
+            
+        }
+    }
+}
